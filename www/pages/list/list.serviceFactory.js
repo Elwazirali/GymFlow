21b83ce9623bf41bb496aaa0df90567cdec6aa54
Elwazirali <elwazirali@gmail.com>
2016-07-31 18:29:32 -0400

    Fixing minor bugs and confirming user creation

--- www/pages/list/list.serviceFactory.js
+++ www/pages/list/list.serviceFactory.js
@@ -2,6 +2,6 @@
  * Created by Ali on 2016-07-24.
  */
 app.factory('Items',function($firebaseArray){
-    var itemsRef = new Firebase("https://gymflow-d2401.firebaseio.com/items");a
+    var itemsRef = new Firebase("https://gymflow-d2401.firebaseio.com/items");
     return $firebaseArray(itemsRef);
 });
--- www/pages/login/login.serviceFactory.js
+++ www/pages/login/login.serviceFactory.js
@@ -9,19 +9,8 @@ app.factory('AddUser',function(){
 
 
   function createUser(){
-    var ref = new Firebase("https://gymflow-d2401.firebaseio.com/users");
-    /*ref.createUser({
-      email    : "elwazirali@gmail.com",
-      password : "correcthorsebatterystaple"
-    }, function(error, userData) {
-      if (error) {
-        console.log("Error creating user:", error);
-      } else {
-        console.log("Successfully created user account with uid:", userData.uid);
-      }
-    });*/
 
-    firebase.auth().createUserWithEmailAndPassword("elwazirali@gmail.com", "777711855mom").catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword("saadsrahimi@gmail.com", "asdfasdfasdf").catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
