b7a2506be03f6024cc464e06244c0acfc194d92a
Elwazirali <elwazirali@gmail.com>
2016-08-01 10:25:00 -0400

    requiring user authentication for all states, except creating user. Merging and resolving conflicts

--- www/app.controller.js
+++ www/app.controller.js
@@ -1,6 +1,22 @@
-app.controller('appCtrl', function ($scope, $ionicModal, localStorageService) {
+app.controller('appCtrl', function ($scope, $ionicModal, $state, $rootScope) {
   var vm = this;
+
+  $rootScope.$on('$stateChangeStart', function(event,toState) {
+    if(firebase.auth().currentUser){
+      console.log(firebase.auth().currentUser);
+    }
+    else if(toState.name == 'signup' || toState.name =='login'){
+      console.log('going to create a user page');
+    }
+    else{
+      $state.go('login');
+      event.preventDefault();
+    }
+  });
+
+
   vm.signout = function(){
+    $state.go('login');
     firebase.auth().signOut().then(function() {
       // Sign-out successful.
     }, function(error) {
--- www/pages/login/login.controller.js
+++ www/pages/login/login.controller.js
@@ -5,15 +5,6 @@
 app.controller('LoginCtrl',function(userAuth,$rootScope,$state){
     var vm = this;
 
-  $rootScope.$on('$stateChangeStart', function(event) {
-    if(firebase.auth().currentUser){
-      console.log(firebase.auth().currentUser);
-    }else{
-      event.preventDefault();
-      alert('user not logged in');
-    }
-  });
-
     vm.login = function(){
       userAuth.signInUser(vm);
     };
